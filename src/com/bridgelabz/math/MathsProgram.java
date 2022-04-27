fae2516b4d5c3ba275463c79b23c67c829b2625d
Thejaswi <thejaswirl187@gmail.com>
2022-04-28 00:22:22 +0530

    Added : Subtraction Program

--- src/com/bridgelabz/math/MathsProgram.java
+++ src/com/bridgelabz/math/MathsProgram.java
@@ -4,7 +4,7 @@ public class MathsProgram {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int add = a+b;
-        System.out.println("The addition is " +add);
+        int sub = a-b;
+        System.out.println("The subtraction is " +sub);
     }
 }
